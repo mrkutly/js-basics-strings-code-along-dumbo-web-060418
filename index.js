d6f877caf506525110ecb035d45eac3fd84943d4
Mark Sauer-Utley <mark.utley.713@gmail.com>
2019-05-01 21:52:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const currentUser = "Grace Hopper";
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
+const currentUser = 'Tony Stark';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const currentUser = "Grace Hopper";
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
